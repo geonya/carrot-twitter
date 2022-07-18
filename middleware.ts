a37633d4a4a2233dba5bbdb06076122b2fecd892
geonya <geony8410@gmail.com>
2022-07-18 13:52:56 +0900

    no middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,3 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest) {
-  if (!req.nextUrl.pathname.startsWith('/log-in')) {
-    if (!req.cookies.get('carrottwitter')) {
-      return NextResponse.redirect(new URL('/log-in', req.url));
-    }
-  }
-
-  return NextResponse.next();
-}
+export async function middleware(req: NextRequest) {}
